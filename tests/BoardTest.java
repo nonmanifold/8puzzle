5d8cd3ad75688bc4bcc548f6e8d896ce77749542
nick serebrennikov <non.manifold@gmail.com>
2016-10-02 11:56:00 +0300

    add board test for toString method

--- tests/BoardTest.java
+++ tests/BoardTest.java
@@ -26,4 +26,31 @@ public class BoardTest {
 
         assertEquals(10, board.manhattan());
     }
+
+    @Test
+    public void toStringTest() {
+        Board board = new Board(new int[][]{
+                {1, 2, 3},
+                {4, 5, 0},
+                {7, 8, 6}
+        });
+
+        assertEquals("3\n" +
+                " 1  2  3 \n" +
+                " 4  5  0 \n" +
+                " 7  8  6 \n", board.toString());
+
+
+        Board board4 = new Board(new int[][]{
+                {0, 12, 9, 13},
+                {15, 11, 10, 14},
+                {3, 7, 5, 6},
+                {4, 8, 2, 1}
+        });
+        assertEquals("4\n" +
+                " 0 12  9 13\n" +
+                "15 11 10 14\n" +
+                " 3  7  5  6\n" +
+                " 4  8  2  1\n", board4.toString());
+    }
 }
